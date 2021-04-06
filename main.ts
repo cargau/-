2bc6799071440b833a3bd0e29f1038e9e0c52bfb
cargau <57837914+cargau@users.noreply.github.com>
2021-04-06 18:00:39 -0400

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,13 @@
+/**
+ * :) ;) :*( :P :D :( :o 8O
+ */
+let rangees = [0, 1, 2, 3, 4]
+let colonnes = [0, 1, 0, 0, 0]
+for (let y of rangees) {
+    for (let x of colonnes) {
+        led.plot(x, y)
+    }
+}
+basic.forever(function () {
+	
+})
